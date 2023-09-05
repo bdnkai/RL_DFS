8fe72677347a6d2f7b128e3f032e8fd0022c71e3
bdnkai <bdn.kai@gmail.com>
2023-09-05 03:17:01 -0700

    include json saver to local dir

--- app/data_script.py
+++ app/data_script.py
@@ -2,39 +2,64 @@ import requests
 import csv
 import json
 import os
-from dotenv import load_dotenv as ld_env
 from modules.dot_export import secrets
 
+# Declares an absolute path to dotenv file
 env_path = os.path.abspath("../.env")
-
+# initiating dot_export module to load .env values
 secret_var = secrets(env_path)
-entry, player_projected_pts, player_actual_pts, team_performance, odds, access = secret_var
+# Unpack secrets as a tuple
+projected_player_pts, actual_player_pts, team_performance, odds, entry, access = secret_var
+# print(secret_var)
+season, week = 2019, 1
 
-print(secret_var)
+filename_mapping = {
+    projected_player_pts: "Player_Projection_Stats",
+    actual_player_pts: "Player_Actual_Stats",
+    team_performance: "Team_Stats",
+    odds: "Game_Odds"
+
+}
+
+def get_filename(endpoint):
+    return filename_mapping.get(endpoint, "default_filename")
+
+# create function to handle apu requests w/ diff yr, wk, and data_types
+def data_requests(target_type, target_season, target_week):
+    # declaring url w/ args for versatile api calls
+    url = f'https://filesamples.com/samples/code/json/sample1.json'
+
+    # url = f'{entry}/{target_type}/{target_season}REG/{target_week}?key={access}'
+    print(f'Data_Type:{target_type},  Uear:{target_season},  WEEK:{target_week}')
+
+    target_response = requests.get(url)
+    target_data = target_response.json()
+
+    assign_tag = get_filename(target_type)
+    print(assign_tag)
+
+    tag_name = f'{target_season}_week-{target_week}__{assign_tag}.json'
+    print(tag_name)
+
+    with open(f'data_json/{tag_name}', 'w') as json_file:
+        json.dump(f'{target_data}', json_file)
+
+    with open(f'data_json/{tag_name}', 'r') as json_file:
+        loaded_data = json.load(json_file)
+
+    if isinstance(loaded_data, list) and loaded_data:
+        fieldnames = loaded_data[0].keys()
+        print(fieldnames)
+        print(loaded_data)
+
+    else:
+        print("Data format not recognized.")
 
 
-# print(f'{entry}{data_one}{}')
-
-
-
+data_requests(projected_player_pts, season, week)
 #
-season = 2019
-week = 1
-#
-url = f'{entry}/{odds}/{season}REG/{week}?key={access}'
-print(url)
 # data_type = "2019REG/1?key="
 #
-# with open('player_stat_by_week.json', 'r') as jsonfile:
-#     data = json.load(jsonfile)
-# fieldnames = data[0].keys()
-# print(fieldnames)
-# print(data)
 #
-# with open('S_2019_G_1_1.csv', 'w', newline='') as csvfile:
-#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#     writer.writeheader()
-#     for row in data:
-#         writer.writerow(row)
-#
+
 # print("CSV conversion completed and saved to 'S_2020_G_1.csv'")
--- app/modules/dot_export.py
+++ app/modules/dot_export.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv as ld_env
 def secrets(path_to_env):
     ld_env(path_to_env)
 
-    entry_point = os.getenv("SECRET_PATH")
     data_point_one = os.getenv("SECRET_A")
     data_point_two = os.getenv("SECRET_B")
     data_pont_three = os.getenv("SECRET_C")
     data_pont_four = os.getenv("SECRET_D")
+    entry_point = os.getenv("SECRET_PATH")
     access_point = os.getenv("SECRET_KEY")
 
-    secret_vars = entry_point, data_point_one, data_point_two, data_pont_three, data_pont_four, access_point
+    secret_vars = data_point_one, data_point_two, data_pont_three, data_pont_four, entry_point, access_point
 
     return secret_vars
